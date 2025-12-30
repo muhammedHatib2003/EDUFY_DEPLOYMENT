c3643c4497dd33380445e9d333c74ee71dcb0874
muuhammedhatib03 <muuhammedhatib039@gmail.com>
2025-12-30 16:08:15 +0300

    fix api base url to include /api

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const apiBase = import.meta.env.VITE_API_URL || 'http://localhost:5001'
+export const apiBase =
+  (import.meta.env.VITE_API_URL || 'http://localhost:5001') + '/api'
 
 export function authedApi(token) {
   return axios.create({
